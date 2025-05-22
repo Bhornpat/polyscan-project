b8264f24dfc0d82a6907bd0f7a828047636a8421
Bhornpat <Thebowiiz2543@gmail.com>
2025-05-22 23:23:59 +0700

    del some files

--- scanner-core/target/release/deps/scanner_core-2a2f777eb9b66bce.d
+++
@@ -1,5 +0,0 @@
-/home/toto/Documents/polyscan-project/scanner-core/target/release/deps/scanner_core-2a2f777eb9b66bce: src/main.rs
-
-/home/toto/Documents/polyscan-project/scanner-core/target/release/deps/scanner_core-2a2f777eb9b66bce.d: src/main.rs
-
-src/main.rs:
--- scanner-core/target/release/scanner-core.d
+++
@@ -1,1 +0,0 @@
-/home/toto/Documents/polyscan-project/scanner-core/target/release/scanner-core: /home/toto/Documents/polyscan-project/scanner-core/src/main.rs
